1b65e7fd4d28f9742900750931d4146d47caef21
MrFibonacc1 <huzaifarehan004@gmail.com>
2023-10-04 20:09:50 -0400

    initial commit

---
+++ Blind 75/Sliding Window/longestSubString.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        map = set()
+        maxString = 0
+        cur = 0
+        l,r = 0,0
+        while r<len(s):
+            if s[r] not in map:
+                map.add(s[r])
+                cur+=1
+                r+=1
+            else:
+                maxString = max(cur,maxString)
+                while(s[r] in map):
+                    cur-=1
+                    map.remove(s[l])
+                    l+=1
+                map.add(s[r])
+                cur+=1
+                r+=1
+            maxString = max(cur,maxString)
+
+        return maxString
+
+solution = Solution()
+
+# Test cases
+result1 = solution.lengthOfLongestSubstring("tearthe")
+print(result1)
