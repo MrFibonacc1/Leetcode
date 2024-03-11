9e5fbd197ecfa5f42161e6a0e026e3dbcc386c9f
MrFibonacc1 <huzaifarehan004@gmail.com>
2024-03-11 17:10:46 -0400

    update

---
+++ Blind75/Arrays_Hashstrings/242.Valing_Anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        dict = {}
+        for i in s:
+            dict[i] = 1 + dict.get(i,0)
+        for i in t:
+            if i in dict and dict[i] > 0:
+                dict[i] -= 1
+            else:
+                return False
+        return True
+
+        #Another way can be to sort each word and compare them
+
+solution = Solution()
+print(solution.isAnagram("anagram","nagaram"))
