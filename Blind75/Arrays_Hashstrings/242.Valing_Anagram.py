40a329c938d78335626a910b6ef659291448c5a3
MrFibonacc1 <huzaifarehan004@gmail.com>
2024-07-10 23:33:09 -0400

    lc 242

--- Blind75/Arrays_Hashstrings/242.Valing_Anagram.py
+++ Blind75/Arrays_Hashstrings/242.Valing_Anagram.py
@@ -1,19 +1,21 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        dict = {}
-        for i in s:
-            dict[i] = 1 + dict.get(i,0)
-        for i in t:
-            if i in dict and dict[i] > 0:
-                dict[i] -= 1
-            else:
-                return False
-        return True
-
-        #Another way can be to sort each word and compare them
-        #Test
-solution = Solution()
-print(solution.isAnagram("anagram","nagaram"))
+    def isAnagram(self, s:str, t:str) -> bool:
+        seen = {}
+        if len(s) == len(t):
+            #O(N)
+            for i in s:
+                if i not in seen:
+                    seen[i] = 1
+                else:
+                    seen[i] += 1
+            #O(N)
+            for i in t:
+                if i in seen and seen[i]>0:
+                    seen[i] -= 1
+                else:
+                    return False
+            return True
+        return False
+    
+Solution = Solution()
+print(Solution.isAnagram("apple","ppleaa"))
