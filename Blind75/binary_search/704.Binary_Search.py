2f7d7f2820b08bb6e22a77276cecab9e098bbdbd
MrFibonacc1 <huzaifarehan004@gmail.com>
2025-03-16 13:38:30 -0400

    5 min

---
+++ Blind75/binary_search/704.Binary_Search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums:list[int], target:int)->int:
+        startIndex = 0
+        endIndex = len(nums)-1
+
+        while endIndex >= startIndex:
+            midpoint = startIndex + (endIndex-startIndex)//2
+            midpointValue = nums[midpoint]
+
+            if midpointValue == target:
+                return midpoint
+            elif midpointValue>target:
+                endIndex = midpoint - 1
+            elif midpointValue < target:
+                startIndex = midpoint + 1
+            
+        return -1
