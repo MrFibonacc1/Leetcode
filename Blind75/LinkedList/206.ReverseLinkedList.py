e9b374853da1e6fb4fc80ec01ce57c31490dd80d
MrFibonacc1 <huzaifarehan004@gmail.com>
2025-04-02 11:45:07 -0400

    review - good

--- Blind75/LinkedList/206.ReverseLinkedList.py
+++ Blind75/LinkedList/206.ReverseLinkedList.py
@@ -7,10 +7,9 @@ class ListNode:
 class Solution:
     def reverseList(self, head:ListNode) ->ListNode:
         prev, curr = None, head
-
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+        while(curr):
+           temp = curr.next
+           curr.next = prev
+           prev = curr
+           curr = temp
         return prev
